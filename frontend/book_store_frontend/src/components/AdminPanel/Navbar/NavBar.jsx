f96293964604f7b947131a289329889f2bda9f1e
Dinh Quan <ntdquan2003@gmail.com>
2024-05-24 12:23:23 +0700

    UI: Adding Base Admin Panel frontend

---
+++ frontend/book_store_frontend/src/components/AdminPanel/Navbar/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '../../../assets/logo.svg'
+import notificationIcon from '../../../assets/notification.svg'
+import Avatar from '../../General/Avatar/Avatar'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <img src={logo} alt="logo" />
+        <span>ADMIN</span>
+      </div>
+      <div className='icons'>
+        <div className='notification'>
+          <img src={notificationIcon} alt=""/>
+          <span>1</span>
+        </div>
+        <div className="user">
+          <Avatar className="user"/>
+          <span>User name</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
